b7c36021bb42240ff0773f743c64d21bac41d3fe
Eugene Naloiko <eugene.naloiko@imito.io>
2025-05-22 17:08:40 +0300

    Adjusted ReactNative sample.

---
+++ SampleRN/ios/WoundGeniusBridge/SampleRN-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  SampleRN-Bridging-Header
+//  SampleRN
+//
+//  Created by Eugene Naloiko on 07.08.2024.
+//  Copyright (c) 2022 by imito AG, Zurich, Switzerland
+//
+
+/**
+ Use this file to import your target's public headers that you would like to expose to Swift.
+ 
+ This file gets auto-created when in Objective C project - Swift files are added.
+*/
+
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventEmitter.h"
